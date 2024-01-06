81c8507c8d04976827bcc24f52f994e25b191b45
Olivera2708 <olivera.radovanovic2002@gmail.com>
2024-01-06 15:00:20 +0100

    #11 add filter done

--- core/core/views.py
+++ core/core/views.py
@@ -8,12 +8,31 @@ def initial(request):
 
 
 def view(request):
+    search = request.POST.get('search')
+    data_source = request.POST.get("data_source")
+    visualizer = request.POST.get("visualizer")
+
+    print("\n\nData Source:", data_source)
+    print("Visualizer:", visualizer)
+    print("\n\n")
+
+    if data_source is None:
+        file_name = "data.json"
+    else:
+        file_name = "data.xml"
+
+    if visualizer is None:
+        visualizer_name = "simple"
+    else:
+        visualizer_name = "block"
+
     config = apps.get_app_config('core')
     config.load_plugins()
     data_source_plugins = config.data_source_plugins
     visualizer_plugins = config.visualizer_plugins
 
-    graph = start_source_plugins(data_source_plugins, "data.json")
+    graph = start_source_plugins(data_source_plugins, file_name)
+    print(graph)
     return redirect('home')
 
 
@@ -21,6 +40,7 @@ def start_source_plugins(source_plugins, file_name):
     for plugin in source_plugins:
         if plugin.name() == file_name.split(".")[1]:
             return plugin.load_data(get_path(file_name))
+
 
 def get_path(file_name):
     file_path = "core/core/static/" + file_name
