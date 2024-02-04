9e9fc27033b69cc3665b26a87ae71a161ccbd98a
Vasilije Marković <markovic.sv15.2021@uns.ac.rs>
2024-02-04 14:04:55 +0100

    #22 Implemented search graph functionality.

--- core/core/views.py
+++ core/core/views.py
@@ -7,6 +7,7 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from jinja2 import FileSystemLoader, Environment
 
+
 def initial(request):
     config = apps.get_app_config('core')
     config.load_plugins()
@@ -75,8 +76,8 @@ def get_current_workspace(request):
     config = apps.get_app_config('core')
     if config.selected_workspace:
         response = {'search_param': config.selected_workspace.search_param,
-                'filter_params': config.selected_workspace.filter_params,
-                'root_node': config.selected_workspace.root_node}
+                    'filter_params': config.selected_workspace.filter_params,
+                    'root_node': config.selected_workspace.root_node}
         print(config.selected_workspace.root_node)
     else:
         response = {}
@@ -84,7 +85,7 @@ def get_current_workspace(request):
 
 
 def view(request):
-    search = request.POST.get('search')
+    search_query = request.POST.get('search').strip()
     visualizer = request.POST.get("visualizer")
     filter_params_request = request.POST.get('filter_params')
     filter_params = None
@@ -105,6 +106,8 @@ def view(request):
 
     try:
         graph = start_source_plugin(data_source_plugins, config.selected_workspace.data_source_id)
+        search_graph(graph, search_query)
+
         html = run_visualisation_plugins(visualizer_plugins, visualizer_name, graph)
         tree_html = load_tree(graph)
         bird_html = load_bird(graph)
@@ -124,18 +127,27 @@ def run_visualisation_plugins(visualisation_plugins, visualizer_name, graph):
     html = template.render(nodes=graph.nodes, edges=graph.edges)
     return html
 
+
 def start_source_plugin(source_plugins, source_id):
     for plugin in source_plugins:
         if plugin.id() == source_id:
             return plugin.load_data(get_path(plugin.file_name()))
 
+
 def load_tree(graph):
     template = get_template("tree.html")
-    return template.render(nodes=graph.nodes, edges=graph.edges, relation=graph.edges[0].type)
+    if graph.edges:
+        relation_type = graph.edges[0].type
+    else:
+        relation_type = None
+
+    return template.render(nodes=graph.nodes, edges=graph.edges, relation=relation_type)
+
 
 def load_bird(graph):
     template = get_template("bird.html")
     return template.render()
+
 
 def get_template(view):
     p = os.path.dirname(__file__)
@@ -144,7 +156,41 @@ def get_template(view):
     template = env.get_template(view)
     return template
 
+
 def get_path(file_name):
     file_path = "core/core/static/" + file_name
     absolute_file_path = os.path.abspath(file_path)
     return absolute_file_path
+
+
+def search_graph(graph, search_query):
+    if search_query == "": return
+    removed_nodes = []
+    for node in graph.nodes:
+        if search_query.upper() in node.name.upper():
+            continue
+        found = False
+        for attribute in node.attributes:
+            if (
+                    search_query.upper() in attribute.name.upper() or
+                    search_query.upper() in attribute.value.upper()
+            ):
+                found = True
+                break
+        if found: continue
+        removed_nodes.append(node)
+
+    if len(removed_nodes) == 0:
+        return
+
+    removed_edges = []
+
+    for edge in graph.edges:
+        if edge.fromNode in removed_nodes or edge.toNode in removed_nodes:
+            removed_edges.append(edge)
+
+    for edge in removed_edges:
+        graph.remove_edge(edge)
+
+    for node in removed_nodes:
+        graph.remove_node(node)
